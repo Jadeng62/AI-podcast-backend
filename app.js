1fbcc61c225ec18b8afc6850bfb4bdc6b215b70c
Jaden <jadenglenn@pursuit.org>
2025-02-05 23:04:23 -0500

    Instillations and foundation set base routes configured

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require("express")
-
+const cors = require("cors")
 const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+
+
+app.post("/generate", (req, res) => {
+    res.send("Generation endpoint test")
+})
 
 app.get("/", (req, res) => {
     res.send("Welcome to Ai-generator backend")
